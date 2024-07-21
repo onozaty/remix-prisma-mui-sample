7c7ddf4414fb11b60dd5fa0741ef1a835bad0856
onozaty <onozaty@gmail.com>
2024-07-21 22:29:41 +0900

    バリデーションで Conform を使うように変更

--- app/routes/customers_.$customerId_.edit.tsx
+++ app/routes/customers_.$customerId_.edit.tsx
@@ -1,8 +1,16 @@
+import { getFieldsetProps, getFormProps, useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
-import { Form, redirect, useLoaderData } from "@remix-run/react";
+import { Form, redirect, useActionData, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
+import { z } from "zod";
 import { getCustomer, updateCustomer } from "../services/customer.server";
+
+const CustomerSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+});
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.customerId, "Missing customerId param");
@@ -17,11 +25,11 @@ export const action = async ({ params, request }: ActionFunctionArgs) => {
   invariant(params.customerId, "Missing customerId param");
 
   const formData = await request.formData();
-  const name = formData.get("name");
-  const email = formData.get("email");
-
-  invariant(typeof name === "string", "name must be a string");
-  invariant(typeof email === "string", "email must be a string");
+  const submission = parseWithZod(formData, { schema: CustomerSchema });
+  if (submission.status !== "success") {
+    return json(submission.reply());
+  }
+  const { name, email } = submission.value;
 
   await updateCustomer({ customerId: Number(params.customerId), name, email });
 
@@ -30,26 +38,46 @@ export const action = async ({ params, request }: ActionFunctionArgs) => {
 
 export default function EditCustomer() {
   const customer = useLoaderData<typeof loader>();
+
+  const lastResult = useActionData<typeof action>();
+  const [form, fields] = useForm({
+    // 前回の送信結果を同期
+    lastResult,
+
+    // クライアントでバリデーション・ロジックを再利用する
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: CustomerSchema });
+    },
+
+    // blurイベント発生時にフォームを検証する
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
   return (
     <Box>
       <Typography variant="h5">Edit Customer</Typography>
       <Box sx={{ mt: 1 }}>
-        <Form method="post">
+        <Form method="post" {...getFormProps(form)}>
           <Grid container spacing={2}>
             <Grid item xs={6}>
               <TextField
                 label="Name"
-                name="name"
                 defaultValue={customer.name}
                 fullWidth
+                error={(fields.name.errors?.length ?? 0) > 0}
+                helperText={fields.name.errors}
+                {...getFieldsetProps(fields.name)}
               />
             </Grid>
             <Grid item xs={6}>
               <TextField
                 label="Email"
-                name="email"
                 defaultValue={customer.email}
                 fullWidth
+                error={(fields.email.errors?.length ?? 0) > 0}
+                helperText={fields.email.errors}
+                {...getFieldsetProps(fields.email)}
               />
             </Grid>
           </Grid>
--- app/routes/customers_.new.tsx
+++ app/routes/customers_.new.tsx
@@ -1,17 +1,23 @@
+import { getFieldsetProps, getFormProps, useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import { ActionFunctionArgs } from "@remix-run/node";
-import { Form, redirect } from "@remix-run/react";
-import invariant from "tiny-invariant";
+import { Form, json, redirect, useActionData } from "@remix-run/react";
+import { z } from "zod";
 import { createCustomer } from "../services/customer.server";
+
+const CustomerSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+});
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
-
-  const name = formData.get("name");
-  const email = formData.get("email");
-
-  invariant(typeof name === "string", "name must be a string");
-  invariant(typeof email === "string", "email must be a string");
+  const submission = parseWithZod(formData, { schema: CustomerSchema });
+  if (submission.status !== "success") {
+    return json(submission.reply());
+  }
+  const { name, email } = submission.value;
 
   await createCustomer({ name, email });
 
@@ -19,17 +25,44 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function NewCustomer() {
+  const lastResult = useActionData<typeof action>();
+  const [form, fields] = useForm({
+    // 前回の送信結果を同期
+    lastResult,
+
+    // クライアントでバリデーション・ロジックを再利用する
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: CustomerSchema });
+    },
+
+    // blurイベント発生時にフォームを検証する
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
   return (
     <Box>
       <Typography variant="h5">New Customer</Typography>
       <Box sx={{ mt: 1 }}>
-        <Form method="post">
+        <Form method="post" {...getFormProps(form)}>
           <Grid container spacing={2}>
             <Grid item xs={6}>
-              <TextField label="Name" name="name" fullWidth />
+              <TextField
+                label="Name"
+                fullWidth
+                error={(fields.name.errors?.length ?? 0) > 0}
+                helperText={fields.name.errors}
+                {...getFieldsetProps(fields.name)}
+              />
             </Grid>
             <Grid item xs={6}>
-              <TextField label="Email" name="email" fullWidth />
+              <TextField
+                label="Email"
+                fullWidth
+                error={(fields.email.errors?.length ?? 0) > 0}
+                helperText={fields.email.errors}
+                {...getFieldsetProps(fields.email)}
+              />
             </Grid>
           </Grid>
           <Box sx={{ justifyContent: "flex-end", display: "flex", mt: 1 }}>
