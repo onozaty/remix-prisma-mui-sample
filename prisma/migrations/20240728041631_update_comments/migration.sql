a3e12c28cdd2891da3f21690ace01c730511af53
onozaty <onozaty@gmail.com>
2024-07-28 12:07:21 +0900

    PrismaでDBコメントも生成
    https://github.com/prisma/prisma/issues/8703

---
+++ prisma/migrations/20240728041631_update_comments/migration.sql
@@ -0,0 +1,24 @@
+-- generator-version: 1.0.0
+
+-- Model customers comments
+
+COMMENT ON COLUMN "customers"."customer_id" IS 'Customer ID';
+COMMENT ON COLUMN "customers"."name" IS 'Customer Name';
+COMMENT ON COLUMN "customers"."email" IS 'e-mail';
+
+
+-- Model products comments
+
+COMMENT ON COLUMN "products"."product_id" IS 'Product ID';
+COMMENT ON COLUMN "products"."name" IS 'Product Name';
+COMMENT ON COLUMN "products"."description" IS 'Product Description';
+COMMENT ON COLUMN "products"."price" IS 'Price';
+
+
+-- Model sales comments
+
+COMMENT ON COLUMN "sales"."sale_id" IS 'Sale ID';
+COMMENT ON COLUMN "sales"."customer_id" IS 'Customer ID';
+COMMENT ON COLUMN "sales"."product_id" IS 'Product ID';
+COMMENT ON COLUMN "sales"."quantity" IS 'Quantity';
+COMMENT ON COLUMN "sales"."total_price" IS 'Total Price';
