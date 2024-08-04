7bd536e3c63dbc8fe70288f6b450239410541f12
onozaty <onozaty@gmail.com>
2024-08-04 23:02:49 +0900

    prisma-db-comments-generator を使ってCOMMENT文作成するように変更

--- prisma/migrations/20240804134901_update_comments/migration.sql
+++ prisma/migrations/20240804134901_update_comments/migration.sql
@@ -1,24 +1,19 @@
--- generator-version: 1.0.0
+-- Prisma Database Comments Generator v1.0.1
 
--- Model customers comments
-
+-- customers comments
 COMMENT ON TABLE "customers" IS 'Customer';
 COMMENT ON COLUMN "customers"."customer_id" IS 'Customer ID';
 COMMENT ON COLUMN "customers"."name" IS 'Customer Name';
 COMMENT ON COLUMN "customers"."email" IS 'e-mail';
 
-
--- Model products comments
-
+-- products comments
 COMMENT ON TABLE "products" IS 'Product';
 COMMENT ON COLUMN "products"."product_id" IS 'Product ID';
 COMMENT ON COLUMN "products"."name" IS 'Product Name';
 COMMENT ON COLUMN "products"."description" IS 'Product Description';
 COMMENT ON COLUMN "products"."price" IS 'Price';
 
-
--- Model sales comments
-
+-- sales comments
 COMMENT ON COLUMN "sales"."sale_id" IS 'Sale ID';
 COMMENT ON COLUMN "sales"."customer_id" IS 'Customer ID';
 COMMENT ON COLUMN "sales"."product_id" IS 'Product ID';
