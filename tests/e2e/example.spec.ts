89488bec07dfd180a90f58782e82b723e5d3395c
onozaty <onozaty@gmail.com>
2024-08-13 21:00:43 +0900

    Revert "Playwrightのセットアップ中"
    This reverts commit 7201f28314dc3d89d66027b74e6aa68c50b92995.
    
    # Conflicts:
    #	.devcontainer/Dockerfile
    #	package-lock.json
    #	package.json

--- tests/e2e/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("has title", async ({ page }) => {
-  await page.goto("http://127.0.0.1:5173/");
-
-  await expect(page).toHaveTitle(/Remix/);
-});
