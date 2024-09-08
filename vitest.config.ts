5c1bb4e39b39fdb62e663a4023668cbff1e1b3bf
onozaty <onozaty@gmail.com>
2024-09-08 16:34:25 +0900

    VSCodeのコード上から実行した際に、環境変数が切り替わらない問題対処。

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,12 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
+import * as dotenv from "dotenv";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+dotenv.config({
+  path: ".env.test",
+});
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
