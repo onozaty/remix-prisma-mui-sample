97c5bcfeb7be684b85edc925a8a28dfbde6e5e52
onozaty <onozaty@gmail.com>
2025-03-16 13:18:50 +0900

    prisma-case-formatを削除し、prisma-schema-fixerを追加してDBマイグレーションスクリプトを更新

---
+++ prisma/schema-fixer.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+/** @type {import("@onozaty/prisma-schema-fixer").Config} */
+export default {
+  rules: {
+    "model-name": [
+      {
+        case: "pascal",
+        form: "singular",
+      },
+    ],
+    "model-map": [
+      {
+        case: "snake",
+        form: "plural",
+      },
+    ],
+    "field-name": [
+      {
+        case: "camel",
+      },
+    ],
+    "field-map": [
+      {
+        case: "snake",
+      },
+    ],
+    "enum-name": [
+      {
+        case: "pascal",
+        form: "singular",
+      },
+    ],
+    "enum-map": [
+      {
+        case: "snake",
+        form: "plural",
+      },
+    ],
+  },
+};
